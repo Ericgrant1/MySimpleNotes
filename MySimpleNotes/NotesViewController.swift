6ce6badbdefd72043cfbd4cc9ad54881f4c583d7
Eric Grant <senatorericgrant@gmail.com>
2021-10-01 01:25:31 +0700

    feat: implement table view for Notes

--- MySimpleNotes/NotesViewController.swift
+++ MySimpleNotes/NotesViewController.swift
@@ -27,6 +27,33 @@ class NotesViewController: UITableViewController {
     }
     
     @objc func insertNewNote(_ sender: Any) {
-        performSegue(withIdentifier: "showCreateNoteSegue", sender: self)
+        performSegue(withIdentifier: "showNewNoteSegue", sender: self)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NoteCellController
+        cell.backgroundColor = UIColor.clear
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+        } else if editingStyle == .insert {
+
+        }
     }
 }
